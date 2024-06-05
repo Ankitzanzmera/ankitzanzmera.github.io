056bffbc86b74cb1bb910d43325a2ac0322fb228
Ankit_zanzmera <zanzmeraankit0@gmail.com>
2024-06-05 06:44:09 +0530

    update

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ let typed = new Typed("#auto-input",{
     backDelay: 100,
     loop: true,
 });
+
+
+const navbar  = document.getElementById("nav-option");
+const links = document.getElementsByTagName("a");
+
+for (let i = 0; i<links.length; i++){
+    links[i].addEventListener("click",function(){
+        const current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active","");
+        this.className ="active";
+    });
+}
